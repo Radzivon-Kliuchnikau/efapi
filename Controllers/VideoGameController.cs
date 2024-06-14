2a74a92813fd71b8bf413b1cdff8517f76e94908
Radzivon-Kliuchnikau <rodionk510@gmail.com>
2024-06-14 10:21:01 +0200

    Add basic controller

---
+++ Controllers/VideoGameController.cs
@@ -0,0 +1,37 @@
+﻿using efapi.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace efapi.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class VideoGameController : ControllerBase
+{
+    [HttpGet]
+    public async Task<IActionResult> GetAllGames()
+    {
+        var videoGames = new List<VideoGame>
+        {
+            new VideoGame
+            {
+                Name = "Red Dead Redemption",
+                Description = "Cool Game",
+                Platforms = new List<Platform>
+                {
+                    new Platform
+                    {
+                        Name = "PS4"
+                    },
+                    new Platform
+                    {
+                        Name = "PC"
+                    }
+                },
+                Genre = "RPG"
+
+            }
+        };
+
+        return Ok(videoGames);
+    }
+}
---
+++ Entities/VideoGame.cs
@@ -0,0 +1,16 @@
+﻿namespace efapi.Entities;
+
+public class VideoGame
+{
+    public string Id { get; set; }
+
+    public required string Name { get; set; }
+
+    public string Genre { get; set; } = string.Empty;
+
+    public string Description { get; set; } = string.Empty;
+
+    public string Publisher { get; set; } = string.Empty;
+
+    public required List<Platform> Platforms { get; set; }
+}
